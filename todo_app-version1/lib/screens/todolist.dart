1f96a384a965cbdaa0ffb53bd524f575dae74553
Abhishek Raj <abhishek.raj@nagarro.com>
2019-01-25 13:02:34 +0530

    todo application 1st version added

---
+++ todo_app-version1/lib/screens/todolist.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/model/todo.dart';
+import 'package:todo_app/screens/tododetails.dart';
+import 'package:todo_app/util/dbhelper.dart';
+
+final List<String> sortingMenu = const <String>[
+  "Sort by Priortiy",
+  "Sort by Date",
+];
+
+const sortPriority = "Sort by Priortiy";
+const sortDate = "Sort by Date";
+
+class TodoList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _TodoList();
+  }
+}
+
+class _TodoList extends State<TodoList> {
+// here my target is to get all todos and its count
+  DBHelper db = DBHelper();
+  List<Todo> _todos;
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_todos == null) {
+      _todos = List<Todo>();
+      getTodosData();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Todo List"),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: selectMenu,
+            itemBuilder: (BuildContext context) {
+              return sortingMenu.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          )
+        ],
+      ),
+      body: todoListItems(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          navigateToDetails(Todo('', 3, '', ''));
+        },
+        tooltip: "Add New Todo",
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void getTodosData() {    
+    final dbFuture = db.initializeDB();
+    dbFuture.then((result) {
+      final todosFuture = db.getTodos();
+      todosFuture.then((result) {
+        List<Todo> todoList = List<Todo>();
+        _count = result.length;
+        for (int i = 0; i < _count; ++i) {          
+          todoList.add(Todo.formObject(result[i]));
+        }
+        setState(() {
+          _todos = todoList;
+          _count = _count;
+        });
+      });
+    });
+  }
+
+  ListView todoListItems() {
+    return ListView.builder(
+      itemCount: _count,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          color: Colors.white,
+          elevation: 1.2,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor:
+                  getColorAccordingToPriority(_todos[index].priority),
+              child: Text(_todos[index].priority.toString()),
+            ),
+            title: Text(_todos[index].title),
+            subtitle: Text(_todos[index].date),
+            onTap: () {
+              navigateToDetails(_todos[index]);
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  Color getColorAccordingToPriority(int priority) {
+    switch (priority) {
+      case 1:
+        return Colors.red;
+        break;
+      case 2:
+        return Colors.orange;
+        break;
+      case 3:
+        return Colors.green;
+        break;
+      default:
+        return Colors.green;
+    }
+  }
+
+  void navigateToDetails(Todo todo) async {
+    bool result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => TodoDetails(todo),
+      ),
+    );
+    if (result) {
+      // if user comes back to the todolist page then update the list from database
+      getTodosData();
+    }
+  }
+
+  void selectMenu(String sortMenuItem) {
+    switch (sortMenuItem) {
+      case sortPriority:
+        print(sortPriority);
+        break;
+      case sortDate:
+        print(sortDate);
+        break;
+    }
+  }
+}
---
+++ todo_app-version1/lib/util/dbhelper.dart
@@ -0,0 +1,78 @@
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:io';
+
+import 'package:todo_app/model/todo.dart';
+
+class DBHelper {
+  static final DBHelper _dbHelper = DBHelper._internal();
+
+  String tblTodo = "todo";
+  String colId = "id";
+  String colTitle = "title";
+  String colDescription = "description";
+  String colDate = "date";
+  String colPriority = "priority";
+
+  DBHelper._internal();
+  
+  factory DBHelper() {
+    return _dbHelper;
+  }
+
+  static Database _db;
+
+  Future<Database> get db async {
+    if (_db == null) {
+      _db = await initializeDB();
+    }
+    return _db;
+  }
+
+  Future<Database> initializeDB() async {
+    Directory dir = await getApplicationDocumentsDirectory(); // the function is in path_provider package. it returns the path of current folder. Although, path is different in IOS and Android but It will work in both case.
+    String path = dir.path + "todos.db";
+    var dbTodos = await openDatabase(path, version: 1, onCreate: _createDb);
+    return dbTodos;
+  }
+
+  void _createDb(Database db, int newVersion) async {
+    await db.execute(
+        "CREATE TABLE $tblTodo ($colId INTEGER PRIMARY KEY, $colTitle TEXT, $colDescription TEXT, $colPriority INTEGER, $colDate TEXT)");
+  }
+
+  Future<int> insertTodo(Todo todo) async {
+    Database db = await this.db;
+    var result = await db.insert(tblTodo, todo.toMap());
+    return result;
+  }
+
+  Future<int> getCount() async{
+    Database db = await this.db;
+    var result = Sqflite.firstIntValue(
+      await db.rawQuery("SELECT COUNT(*) FROM $tblTodo")
+    );
+    return result;
+  }
+
+  Future<List> getTodos() async {
+    Database db = await this.db;
+    var result =
+        await db.rawQuery("SELECT * FROM $tblTodo ORDER BY $colPriority ASC");
+    return result;
+  }
+
+  Future<int> updateTodo(Todo todo) async {
+    Database db = await this.db;
+    var result = await db.update(tblTodo, todo.toMap(),
+        where: "$colId = ?", whereArgs: [todo.id]);
+    return result;
+  }
+
+  Future<int> deleteTodo(int id) async {
+    Database db = await this.db;
+    // var result = await db.rawDelete("DELETE FROM $tblTodo WEHRE $colId = $id");
+    var result = await db.delete(tblTodo, where: "$colId = ?", whereArgs: [id]);
+    return result;
+  }
+}
